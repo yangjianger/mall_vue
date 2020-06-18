261cfac80011539fa3cd87f2c5cef7a993a42529
yangjiang <1084914120@qq.com>
2020-06-18 14:41:48 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-06-18 14:36:57
+ * @LastEditTime: 2020-06-18 14:40:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vue/webpack/webpack-cli/supermall/src/App.vue
+--> 
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   aaa
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
